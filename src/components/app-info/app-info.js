b3448c67461819fe6939d26e65ef9829e33448e1
tonyWh1te <zmyshenko.valery@bk.ru>
2023-01-10 18:12:30 +0300

    creating components

---
+++ src/components/app-info/app-info.js
@@ -0,0 +1,13 @@
+import './app-info.css';
+
+const AppInfo = () => {
+  return (
+    <div className="app-info">
+      <h1>Учет сотрудников в компании Microsoft</h1>
+      <h3>Общее число сотрудников: </h3>
+      <h3>Премию получат: </h3>
+    </div>
+  );
+};
+
+export default AppInfo;
---
+++ src/components/search-panel/search-panel.js
@@ -0,0 +1,7 @@
+import './search-panel.css';
+
+const SearchPanel = () => {
+  return <input className="form-control search-input" type="text" placeholder="Найти сотрудника" />;
+};
+
+export default SearchPanel;
