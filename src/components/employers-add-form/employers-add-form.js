6f0610fc7872c8508c2a572b3767bdd2a3a8bb45
tonyWh1te <zmyshenko.valery@bk.ru>
2023-01-14 16:20:34 +0300

    form state tracking

--- src/components/employers-add-form/employers-add-form.js
+++ src/components/employers-add-form/employers-add-form.js
@@ -1,19 +1,53 @@
+import { Component } from 'react';
 import './employers-add-form.css';
 
-const EmployerAddForm = () => {
-  return (
-    <div className="app-add-form">
-      <h3>Добавьте нового сотрудника</h3>
-      <form className="add-form d-flex">
-        <input type="text" className="form-control new-post-label" placeholder="Как его зовут?" />
-        <input type="number" className="form-control new-post-label" placeholder="З/П в $?" />
+class EmployerAddForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      salary: '',
+    };
+  }
 
-        <button type="submit" className="btn btn-outline-light">
-          Добавить
-        </button>
-      </form>
-    </div>
-  );
-};
+  onValueChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  render() {
+    const { name, salary } = this.state;
+
+    return (
+      <div className="app-add-form">
+        <h3>Добавьте нового сотрудника</h3>
+        <form className="add-form d-flex">
+          {/* сделали инпуты управляемыми компонентами за счет добавления атрибута value. Теперь реакт не только меняет сосотояние, но и отслеживает значение формы */}
+          <input
+            className="form-control new-post-label"
+            type="text"
+            placeholder="Как его зовут?"
+            name="name"
+            value={name}
+            onChange={this.onValueChange}
+          />
+          <input
+            className="form-control new-post-label"
+            type="number"
+            placeholder="З/П в $?"
+            name="salary"
+            value={salary}
+            onChange={this.onValueChange}
+          />
+
+          <button type="submit" className="btn btn-outline-light">
+            Добавить
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
 
 export default EmployerAddForm;
